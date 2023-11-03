7d0b486a96955b9c2ac075bf135d30e7937c77bc
starsasdust <47842264+starsasdust@users.noreply.github.com>
2023-11-03 00:27:57 -0400

    Add files via upload

---
+++ 1.CameraCap.py
@@ -0,0 +1,36 @@
+import cv2
+
+# Initialize the camera
+cap = cv2.VideoCapture(0)  # '0' is the default value for the primary camera
+
+# Set the resolution
+desired_width = 1920
+desired_height = 1080
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, desired_width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, desired_height)
+
+# Counter for the image name
+counter = 0
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imshow('Enter to Capture, Esc to Quit', frame)
+
+    # Wait for the Enter or Esc key to be pressed
+    key = cv2.waitKey(1)
+    if key == 13:  # 13 is the Enter Key
+        img_name = f"opencv_frame_{counter}.png"
+        cv2.imwrite(img_name, frame)
+        print(f"{img_name} written!")
+        counter += 1
+
+    # Break the loop with the 'Esc' key
+    if key == 27:  # 27 is the Esc Key
+        break
+
+# When everything is done, release the capture
+cap.release()
+cv2.destroyAllWindows()
---
+++ undistortion.py
@@ -0,0 +1,36 @@
+import numpy as np
+import cv2
+import glob
+
+# Load previously saved calibration data
+with np.load('calibration_data.npz') as X:
+    mtx, dist = [X[i] for i in ('mtx', 'dist')]
+
+# Define the path to the images you want to undistort
+images_path = "D:\\openCV\\*.png" 
+# Use glob to get all the images with .png extension
+images = glob.glob(images_path)
+
+# Check if images list is empty
+if not images:
+    print("No images found. Check the file path and extension.")
+else:
+    print(f"Found {len(images)} images for undistortion.")
+
+# Starting undistortion process
+for i, fname in enumerate(images):
+    img = cv2.imread(fname)
+    h, w = img.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+    # Undistort
+    dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+
+    # Crop the image
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+    
+    # Save the undistorted image
+    cv2.imwrite(f"D:\\openCV\\undistorted_{i}.png", dst)
+
+print("Undistortion completed. Undistorted images saved.")
